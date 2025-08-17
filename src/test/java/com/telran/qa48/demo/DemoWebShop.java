5f8f89bab0c49afceb9edbd8c6d4eceb913bd4e7
Evgeny Polonsky <evgpolon@gmail.com>
2025-08-17 21:19:47 +0300

    first add

---
+++ src/test/java/com/telran/qa48/demo/DemoWebShop.java
@@ -0,0 +1,24 @@
+package com.telran.qa48.demo;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class DemoWebShop {
+
+    WebDriver driver;
+
+@BeforeMethod
+public void setup() {
+    driver = new ChromeDriver();
+    driver.get("https://demowebshop.tricentis.com/");
+}
+
+
+    @Test
+    public void openShop() {
+        System.out.println("WEBSHOP is opened");
+    }
+
+}
