a54c0e07658ac70c541588c7f0bf0944b0b5ad45
Evgeny Polonsky <evgpolon@gmail.com>
2025-08-30 15:18:14 +0300

    HW_6_Completed_1.3 added another one test deleteFromCartCustomQuantityTest

--- src/test/java/com/telran/qa48/demo/DeleteFromCart.java
+++ src/test/java/com/telran/qa48/demo/DeleteFromCart.java
@@ -1,12 +1,13 @@
 package com.telran.qa48.demo;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 
-public class DeleteFromCart extends TestBase{
+public class DeleteFromCart extends TestBase {
 
     @BeforeMethod
     public void preCondition() {
@@ -21,7 +22,7 @@ public class DeleteFromCart extends TestBase{
     }
 
     @Test
-    public void deleteAllFromCartTest () {
+    public void deleteAllFromCartTest() {
         click(By.linkText("Shopping cart"));
         int sizeBefore = sizeInCart();
         click(By.name("removefromcart"));
@@ -31,7 +32,35 @@ public class DeleteFromCart extends TestBase{
         Assert.assertEquals(sizeAfter, sizeBefore - 1);
     }
 
+    public String qtyBefore, qtyAfter;
+    @Test
+    public void deleteFromCartCustomQuantityTest() {
+        click(By.linkText("Shopping cart"));
+        pause(1000);
+        qtyBefore = qtyInCart();
+        type(By.className("qty-input"), qtyToDel());
+        pause(1000);
+        click(By.name("updatecart"));
+        pause(1000);
+        qtyAfter = qtyInCart();
+        int before = Integer.parseInt(qtyBefore);
+        int after = Integer.parseInt(qtyAfter);
+        Assert.assertEquals(after, before - 1);
 
+
+    }
+
+    private String qtyInCart() {
+        WebElement input = driver.findElement(By.className("qty-input"));
+        return input.getAttribute("value");
+
+    }
+
+    public String qtyToDel() {
+        int qtyToDelete = Integer.parseInt(qtyBefore) - 1;
+        String qtyToDel = Integer.toString(qtyToDelete);
+        return qtyToDel;
+    }
 
     private int sizeInCart() {
         if(isElementPresent(By.xpath("//td[@class='product']"))){
@@ -48,18 +77,10 @@ public class DeleteFromCart extends TestBase{
         }
     }
 
+
+
 }
 
 /*
-@Test
-    public void deleteFromCartCustomQuantityTest () {
-        click(By.linkText("Shopping cart"));
-        String sToDelete = Integer.toString(sizeToDelete);
-        type(By.className("qty-input"),sToDelete );
-        click(By.name("removefromcart"));
-        click(By.name("updatecart"));
-        pause(1000);
-        int sizeAfter = sizeInCart();
-        Assert.assertEquals(sizeAfter, sizeBefore - 1);
-    }
+
  */
